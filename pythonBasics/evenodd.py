b59f46740a4524a06739413ba34df1cf7a03326b
Sagar <sagarspawar0707@gmail.com>
2024-01-01 22:32:44 +0530

    Architect Asian Development

---
+++ pythonBasics/evenodd.py
@@ -0,0 +1,8 @@
+a =int(input("Enter any number"))
+b =int(input("Enter any number"))
+
+print("{} {} {} ".format("before swaping value is", a,b))
+
+a,b=b,a
+
+print("{} {} {} ".format("after swaping value is", a,b))
